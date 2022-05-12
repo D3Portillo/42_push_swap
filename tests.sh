e18b7d33562cf3e4b058948eb22498573aeaac33
D3Portillo <d3portillo@gmail.com>
2022-05-12 23:32:34 +0200

    chores(tests.sh): fail action when test KOs

--- tests.sh
+++ tests.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 HR="----------------------------------------"
+total=0
+status_ko=0
 
-cecho (){
+cecho () {
   RED="\033[0;31m"
   GREEN="\033[0;32m"
   YELLOW="\033[1;33m"
@@ -19,6 +21,17 @@ describe () {
   cecho "CYAN" $HR
 }
 
+get_results () {
+  describe "TEST RESULTS"
+  cecho "NC"    "TOTAL: $total"
+  cecho "GREEN" "OKs:   $(($total - $status_ko))"
+  if [ $status_ko -gt 0 ]
+  then
+    cecho "RED"   "KOs:   $status_ko"
+    exit 1
+  fi
+}
+
 test () {
   result=$(./push_swap $1)
   echo "Input: $1"
@@ -29,13 +42,15 @@ test () {
   else
     echo "Empty"
   fi
-  checker=$(./push_swap $1 | ./checker_linux $1 2>/dev/null)
+  checker=$(./push_swap $1 2>/dev/null | ./checker_linux $1 2>/dev/null)
   echo -n "Status: "
-  if [[ "$checker" == "OK" ]]
+  ((total++))
+  if [[ "$checker" == "OK" || "$checker" == "" ]]
   then
     cecho "GREEN" "OK"
   else
     cecho "RED" "KO"
+    ((status_ko++))
   fi
   echo
 }
@@ -43,10 +58,40 @@ test () {
 describe "MAKEFILE"
 make
 
+describe "[SIZE=1] Empty"
+test "1"
+test "0"
+test "-1"
+
+describe "[SIZE=1] Wrong format"
+test "1 a"
+test "0 a"
+test "-1 9999999999"
+
+describe "[SIZE=2] No sort"
+test "1 2"
+test "0 1"
+test "-1 4"
+
+describe "[SIZE=2] No sort"
+test "2 1"
+test "3 1"
+test "-2 -1"
+
 describe "[SIZE=3] No sort"
 test "1 2 3"
+test "0 0 0"
+test "-1 4 20"
 
-describe "[SIZE=3] Index 2 has the greatest value"
-test "5 1 2"
-test "3 2 1"
-test "3 3 3"
+describe "[SIZE=3] Index 1 has the greatest value"
+test "1 6 3"
+test "0 42 3"
+test "-1 0 -2"
+test "-42 42 20"
+
+describe "[SIZE=3] Index 0 has the greatest value"
+test "9 3 6"
+test "9 0 0"
+test "4 2 0"
+
+get_results
